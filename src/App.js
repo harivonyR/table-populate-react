b3897510a36cdcee26386a000bab592285455211
TensorEleven <harivonyratefiarison@gmail.com>
2022-03-24 10:04:04 +0300

    CHECK react-bootstrap linked, data to table load

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
+  const data = [
+    {name:"Town House",location:"Lagos",link:"http://lagos-preperty"},
+    {name:"Appartement",location:"Caraib",link:"http://lagos-preperty"},
+    {name:"Country side",location:"Minsk",link:"http://lagos-preperty"}
+  ]
+
+  const dataRender = (data,index)=>{
+    return(
+      <tr key = {index}>
+        <td>{data.name}</td>
+        <td>{data.location}</td>
+        <td>{data.link}</td>
+      </tr>
+      
+    )
+  } 
   return (
-    <>
-      <h1>Web Scraping App</h1>
-    </>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Location</th>
+          <th>Link</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(dataRender)}
+      </tbody>
+  </Table>
   );
 }
 
